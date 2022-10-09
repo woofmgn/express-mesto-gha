a945f5d33d190e7499c6ba55ed1abed270074111
Denis Vakulenko <woof88@yandex.ru>
2022-10-09 09:42:32 +0600

    added: edit user

--- controllers/users.js
+++ controllers/users.js
@@ -2,13 +2,13 @@ const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.send({ data: users }))
+    .then((users) => res.send(users))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
 module.exports.getUser = (req, res) => {
   User.findById(req.params.id)
-    .then((user) => res.send({ data: user }))
+    .then((user) => res.send(user))
     .catch((err) => res.status(500).send({ message: `Произошла ошибка: ${err.message}` }));
 };
 
@@ -18,4 +18,10 @@ module.exports.createUser = (req, res) => {
   User.create({ name, about, avatar })
     .then((user) => res.send(user))
     .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.editUser = (req, res) => {
+  User.findByIdAndUpdate(req.user._id, req.body, { new: true })
+    .then((user) => res.send(user))
+    .catch((err) => res.send(`Произошла ошибка: ${err.message}`));
 };
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 const userRouter = require('express').Router();
-const { getUsers, getUser, createUser } = require('../controllers/users');
+const {
+  getUsers, getUser, createUser, editUser,
+} = require('../controllers/users');
 
 userRouter.get('/users', getUsers);
 userRouter.get('/users/:id', getUser);
 userRouter.post('/users', createUser);
+userRouter.patch('/users/me', editUser);
 
 module.exports = userRouter;
