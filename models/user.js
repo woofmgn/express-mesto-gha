009a4c52c5926cb62be463ce60d5d85e9de88793
Denis Vakulenko <woof88@yandex.ru>
2022-10-14 00:15:07 +0500

    1

--- models/user.js
+++ models/user.js
@@ -4,14 +4,14 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     require: true,
-    minlength: 2,
-    maxlength: 30,
+    minlength: [2, 'Минимальная длина - 2 символа'],
+    maxlength: [30, 'Максимальная длина - 30 символов'],
   },
   about: {
     type: String,
     require: true,
-    minlength: 2,
-    maxlength: 30,
+    minlength: [2, 'Минимальная длина - 2 символа'],
+    maxlength: [30, 'Максимальная длина - 30 символов'],
   },
   avatar: {
     type: String,
