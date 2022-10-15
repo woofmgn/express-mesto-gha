f3c91b323407e4e2c5077dc65c22970fc5866614
Denis Vakulenko <woof88@yandex.ru>
2022-10-15 22:49:53 +0500

    fix: err.message

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,15 +1,17 @@
-const Card = require('../models/card');
+const Card = require("../models/card");
 
 const {
   ERROR_CODE_INCORRECT_DATA,
   ERROR_CODE_DATA_NOT_FOUND,
   ERROR_CODE_DEFAULT,
-} = require('../utills/utills');
+} = require("../utills/utills");
 
 module.exports.getCards = (req, res) => {
   Card.find({})
     .then((cards) => res.send(cards))
-    .catch((err) => res.status(ERROR_CODE_DEFAULT).send({ message: `Произошла ошибка: ${err.message}` }));
+    .catch(() =>
+      res.status(ERROR_CODE_DEFAULT).send({ message: "Произошла ошибка" })
+    );
 };
 
 module.exports.createCard = (req, res) => {
@@ -18,11 +20,16 @@ module.exports.createCard = (req, res) => {
   Card.create({ name, link, owner: req.user._id })
     .then((cards) => res.send(cards))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(ERROR_CODE_INCORRECT_DATA).send({ message: `Переданы некорректные данные при создании карточки, произошла ошибка: ${err.message}` });
+      if (err.name === "ValidationError") {
+        res
+          .status(ERROR_CODE_INCORRECT_DATA)
+          .send({
+            message:
+              "Переданы некорректные данные при создании карточки, произошла ошибка",
+          });
         return;
       }
-      res.status(ERROR_CODE_DEFAULT).send({ message: `Произошла ошибка: ${err.message}` });
+      res.status(ERROR_CODE_DEFAULT).send({ message: "Произошла ошибка" });
     });
 };
 
@@ -30,21 +37,30 @@ module.exports.deleteCard = (req, res) => {
   Card.findById(req.params.cardId)
     .then((card) => {
       if (!card) {
-        res.status(ERROR_CODE_DATA_NOT_FOUND).send({ message: 'Карточка с указанным _id не найдена' });
+        res
+          .status(ERROR_CODE_DATA_NOT_FOUND)
+          .send({ message: "Карточка с указанным _id не найдена" });
         return;
       }
       Card.findByIdAndRemove(req.params.cardId)
-        .then(() => res.send({ message: `Карточка ${req.params.cardId} удалена` }))
-        .catch((err) => {
-          res.status(ERROR_CODE_DEFAULT).send({ message: `Ошибка ${err.message}` });
+        .then(() =>
+          res.send({ message: `Карточка ${req.params.cardId} удалена` })
+        )
+        .catch(() => {
+          res.status(ERROR_CODE_DEFAULT).send({ message: "Произошла ошибка" });
         });
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(ERROR_CODE_INCORRECT_DATA).send({ message: `Переданы некорректные данные для удаления карточки, произошла ошибка: ${err.message}` });
+      if (err.name === "CastError") {
+        res
+          .status(ERROR_CODE_INCORRECT_DATA)
+          .send({
+            message:
+              "Переданы некорректные данные для удаления карточки, произошла ошибка",
+          });
         return;
       }
-      res.status(ERROR_CODE_DEFAULT).send({ message: `Ошибка ${err.message}` });
+      res.status(ERROR_CODE_DEFAULT).send({ message: "Произошла ошибка" });
     });
 };
 
@@ -52,22 +68,33 @@ module.exports.likeCard = (req, res) => {
   Card.findById(req.params.cardId)
     .then((card) => {
       if (!card) {
-        res.status(ERROR_CODE_DATA_NOT_FOUND).send({ message: 'Карточка с указанным _id не найдена' });
+        res
+          .status(ERROR_CODE_DATA_NOT_FOUND)
+          .send({ message: "Карточка с указанным _id не найдена" });
         return;
       }
       // eslint-disable-next-line max-len
-      Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
+      Card.findByIdAndUpdate(
+        req.params.cardId,
+        { $addToSet: { likes: req.user._id } },
+        { new: true }
+      )
         .then((newCard) => res.send(newCard))
-        .catch((err) => {
-          res.status(ERROR_CODE_DEFAULT).send({ message: `Произошла ошибка ${err.message}` });
+        .catch(() => {
+          res.status(ERROR_CODE_DEFAULT).send({ message: "Произошла ошибка" });
         });
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(ERROR_CODE_INCORRECT_DATA).send({ message: `Переданы некорректные данные для постановки/снятии лайка, произошла ошибка: ${err.message}` });
+      if (err.name === "CastError") {
+        res
+          .status(ERROR_CODE_INCORRECT_DATA)
+          .send({
+            message:
+              "Переданы некорректные данные для постановки/снятии лайка, произошла ошибка",
+          });
         return;
       }
-      res.status(ERROR_CODE_DEFAULT).send({ message: `Ошибка ${err.message}` });
+      res.status(ERROR_CODE_DEFAULT).send({ message: "Произошла ошибка" });
     });
 };
 
@@ -75,20 +102,31 @@ module.exports.dislikeCard = (req, res) => {
   Card.findById(req.params.cardId)
     .then((card) => {
       if (!card) {
-        res.status(ERROR_CODE_DATA_NOT_FOUND).send({ message: 'Карточка с указанным _id не найдена' });
+        res
+          .status(ERROR_CODE_DATA_NOT_FOUND)
+          .send({ message: "Карточка с указанным _id не найдена" });
         return;
       }
-      Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
+      Card.findByIdAndUpdate(
+        req.params.cardId,
+        { $pull: { likes: req.user._id } },
+        { new: true }
+      )
         .then((newCard) => res.send(newCard))
-        .catch((err) => {
-          res.status(ERROR_CODE_DEFAULT).send({ message: `Произошла ошибка ${err.message}` });
+        .catch(() => {
+          res.status(ERROR_CODE_DEFAULT).send({ message: "Произошла ошибка" });
         });
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(ERROR_CODE_INCORRECT_DATA).send({ message: `Переданы некорректные данные для постановки/снятии лайка, произошла ошибка: ${err.message}` });
+      if (err.name === "CastError") {
+        res
+          .status(ERROR_CODE_INCORRECT_DATA)
+          .send({
+            message:
+              "Переданы некорректные данные для постановки/снятии лайка, произошла ошибка",
+          });
         return;
       }
-      res.status(ERROR_CODE_DEFAULT).send({ message: `Ошибка ${err.message}` });
+      res.status(ERROR_CODE_DEFAULT).send({ message: "Произошла ошибка" });
     });
 };
