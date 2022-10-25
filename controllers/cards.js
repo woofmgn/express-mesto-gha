0136d706a0e807e8c82beb91f252d25744172292
Denis VakulenkoDenis <woof88@yandex.ruwoof88@yandex.ru>
2022-10-25 10:31:22 +0500

    fix

--- controllers/cards.js
+++ controllers/cards.js
@@ -19,7 +19,10 @@ module.exports.createCard = (req, res) => {
     .then((cards) => res.send(cards))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(ERROR_CODE_INCORRECT_DATA).send({ message: 'Переданы некорректные данные при создании карточки, произошла ошибка' });
+        res.status(ERROR_CODE_INCORRECT_DATA).send({
+          message:
+            'Переданы некорректные данные при создании карточки, произошла ошибка',
+        });
         return;
       }
       res.status(ERROR_CODE_DEFAULT).send({ message: 'Произошла ошибка' });
@@ -30,7 +33,9 @@ module.exports.deleteCard = (req, res) => {
   Card.findById(req.params.cardId)
     .then((card) => {
       if (!card) {
-        res.status(ERROR_CODE_DATA_NOT_FOUND).send({ message: 'Карточка с указанным _id не найдена' });
+        res
+          .status(ERROR_CODE_DATA_NOT_FOUND)
+          .send({ message: 'Карточка с указанным _id не найдена' });
         return;
       }
       Card.findByIdAndRemove(req.params.cardId)
@@ -41,7 +46,10 @@ module.exports.deleteCard = (req, res) => {
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(ERROR_CODE_INCORRECT_DATA).send({ message: 'Переданы некорректные данные для удаления карточки, произошла ошибка' });
+        res.status(ERROR_CODE_INCORRECT_DATA).send({
+          message:
+            'Переданы некорректные данные для удаления карточки, произошла ошибка',
+        });
         return;
       }
       res.status(ERROR_CODE_DEFAULT).send({ message: 'Произошла ошибка' });
@@ -52,7 +60,9 @@ module.exports.likeCard = (req, res) => {
   Card.findById(req.params.cardId)
     .then((card) => {
       if (!card) {
-        res.status(ERROR_CODE_DATA_NOT_FOUND).send({ message: 'Карточка с указанным _id не найдена' });
+        res
+          .status(ERROR_CODE_DATA_NOT_FOUND)
+          .send({ message: 'Карточка с указанным _id не найдена' });
         return;
       }
       Card.findByIdAndUpdate(
@@ -67,7 +77,10 @@ module.exports.likeCard = (req, res) => {
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(ERROR_CODE_INCORRECT_DATA).send({ message: 'Переданы некорректные данные для постановки/снятии лайка, произошла ошибка' });
+        res.status(ERROR_CODE_INCORRECT_DATA).send({
+          message:
+            'Переданы некорректные данные для постановки/снятии лайка, произошла ошибка',
+        });
         return;
       }
       res.status(ERROR_CODE_DEFAULT).send({ message: 'Произошла ошибка' });
@@ -78,10 +91,16 @@ module.exports.dislikeCard = (req, res) => {
   Card.findById(req.params.cardId)
     .then((card) => {
       if (!card) {
-        res.status(ERROR_CODE_DATA_NOT_FOUND).send({ message: 'Карточка с указанным _id не найдена' });
+        res
+          .status(ERROR_CODE_DATA_NOT_FOUND)
+          .send({ message: 'Карточка с указанным _id не найдена' });
         return;
       }
-      Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
+      Card.findByIdAndUpdate(
+        req.params.cardId,
+        { $pull: { likes: req.user._id } },
+        { new: true },
+      )
         .then((newCard) => res.send(newCard))
         .catch(() => {
           res.status(ERROR_CODE_DEFAULT).send({ message: 'Произошла ошибка' });
@@ -89,7 +108,10 @@ module.exports.dislikeCard = (req, res) => {
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(ERROR_CODE_INCORRECT_DATA).send({ message: 'Переданы некорректные данные для постановки/снятии лайка, произошла ошибка' });
+        res.status(ERROR_CODE_INCORRECT_DATA).send({
+          message:
+            'Переданы некорректные данные для постановки/снятии лайка, произошла ошибка',
+        });
         return;
       }
       res.status(ERROR_CODE_DEFAULT).send({ message: 'Произошла ошибка' });
--- controllers/users.js
+++ controllers/users.js
@@ -16,14 +16,18 @@ module.exports.getUser = (req, res) => {
   User.findById(req.params.id)
     .then((user) => {
       if (!user) {
-        res.status(ERROR_CODE_DATA_NOT_FOUND).send({ message: 'Пользователь с указанным _id не найден, произошла ошибка' });
+        res.status(ERROR_CODE_DATA_NOT_FOUND).send({
+          message: 'Пользователь с указанным _id не найден, произошла ошибка',
+        });
         return;
       }
       res.send(user);
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(ERROR_CODE_INCORRECT_DATA).send({ message: 'Передан не верный id пользователя, произошла ошибка' });
+        res.status(ERROR_CODE_INCORRECT_DATA).send({
+          message: 'Передан не верный id пользователя, произошла ошибка',
+        });
         return;
       }
       res.status(ERROR_CODE_DEFAULT).send({ message: 'Произошла ошибка' });
@@ -37,7 +41,10 @@ module.exports.createUser = (req, res) => {
     .then((user) => res.send(user))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(ERROR_CODE_INCORRECT_DATA).send({ message: 'Переданы некорректные данные при создании пользователя, произошла ошибка' });
+        res.status(ERROR_CODE_INCORRECT_DATA).send({
+          message:
+            'Переданы некорректные данные при создании пользователя, произошла ошибка',
+        });
         return;
       }
       res.status(ERROR_CODE_DEFAULT).send({ message: 'Произошла ошибка' });
@@ -47,16 +54,25 @@ module.exports.createUser = (req, res) => {
 module.exports.editUser = (req, res) => {
   const { name, about } = req.body;
 
-  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    { new: true, runValidators: true },
+  )
     .orFail(new Error('NotFound'))
     .then((user) => res.send(user))
     .catch((err) => {
       if (err.name === 'NotFound') {
-        res.status(ERROR_CODE_DATA_NOT_FOUND).send({ message: 'Пользователь с указанным _id не найден, произошла ошибка' });
+        res.status(ERROR_CODE_DATA_NOT_FOUND).send({
+          message: 'Пользователь с указанным _id не найден, произошла ошибка',
+        });
         return;
       }
       if (err.name === 'ValidationError') {
-        res.status(ERROR_CODE_INCORRECT_DATA).send({ message: 'Переданы некорректные данные при обновлении профиля, произошла ошибка' });
+        res.status(ERROR_CODE_INCORRECT_DATA).send({
+          message:
+            'Переданы некорректные данные при обновлении профиля, произошла ошибка',
+        });
         return;
       }
       res.status(ERROR_CODE_DEFAULT).send({ message: 'Произошла ошибка' });
@@ -66,16 +82,25 @@ module.exports.editUser = (req, res) => {
 module.exports.editAvatar = (req, res) => {
   const { avatar } = req.body;
 
-  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    { new: true, runValidators: true },
+  )
     .orFail(new Error('NotFound'))
     .then((userAvatar) => res.send(userAvatar))
     .catch((err) => {
       if (err.name === 'NotFound') {
-        res.status(ERROR_CODE_DATA_NOT_FOUND).send({ message: 'Пользователь с указанным _id не найден, произошла ошибка' });
+        res.status(ERROR_CODE_DATA_NOT_FOUND).send({
+          message: 'Пользователь с указанным _id не найден, произошла ошибка',
+        });
         return;
       }
       if (err.name === 'ValidationError') {
-        res.status(ERROR_CODE_INCORRECT_DATA).send({ message: 'Переданы некорректные данные при обновлении профиля, произошла ошибка' });
+        res.status(ERROR_CODE_INCORRECT_DATA).send({
+          message:
+            'Переданы некорректные данные при обновлении профиля, произошла ошибка',
+        });
         return;
       }
       res.status(ERROR_CODE_DEFAULT).send({ message: 'Произошла ошибка' });
