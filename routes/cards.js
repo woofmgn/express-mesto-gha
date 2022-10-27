abe756c0e0c2469ff9bdd08598f02fd2265489f2
Denis VakulenkoDenis <woof88@yandex.ruwoof88@yandex.ru>
2022-10-27 17:38:17 +0500

    fix: validationCreateCard

--- routes/cards.js
+++ routes/cards.js
@@ -3,10 +3,11 @@ const auth = require('../middlewares/auth');
 const {
   getCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
+const { validationCreateCard } = require('../middlewares/validationJoiCard');
 
 cardRouter.use(auth);
 cardRouter.get('/cards', getCards);
-cardRouter.post('/cards', createCard);
+cardRouter.post('/cards', validationCreateCard, createCard);
 cardRouter.delete('/cards/:cardId', deleteCard);
 cardRouter.put('/cards/:cardId/likes', likeCard);
 cardRouter.delete('/cards/:cardId/likes', dislikeCard);
