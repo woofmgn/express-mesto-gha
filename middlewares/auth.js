58a28105840d7cb7e9d4c6049c13d9deef21d9bb
Denis Vakulenko <woof88@yandex.ru>
2022-10-28 01:02:59 +0500

    fix: global validation for auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const { IncorrectTokenError } = require('../errors/incorrectTokenError');
+const IncorrectTokenError = require('../errors/incorrectTokenError');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
