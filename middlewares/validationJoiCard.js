6302dfe9d442d87d6427c6c0f04171d2a7e7c7fc
Denis VakulenkoDenis <woof88@yandex.ruwoof88@yandex.ru>
2022-10-27 17:58:42 +0500

    fix: link required()

--- middlewares/validationJoiCard.js
+++ middlewares/validationJoiCard.js
@@ -4,7 +4,7 @@ const { REGEX_URL } = require('../utills/utills');
 module.exports.validationCreateCard = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().pattern(REGEX_URL),
+    link: Joi.string().required().pattern(REGEX_URL),
   }),
 });
 
